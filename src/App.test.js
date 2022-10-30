813eff89b64f2eab91a147607c34ae36a1a3563d
Agustin Tisera <agustisera1@gmail.com>
2022-10-30 16:36:28 -0300

    instala react-testing-library, actualiza versiones react

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
-test('renders learn react link', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Renderiza app', () => {
+  const { getByTestId } = render(<App />);
+  const appContainer = getByTestId('contenedor-app');
+  expect(appContainer).toBeInTheDocument()
 });
---
+++ src/components/Listado.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Listado = () => {
+  return (
+    <div data-qa="componente-listado" className="App">
+      <header className="App-listado">
+        <a
+          className="App-link"
+          href={`/`}
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          {`< Volver`}
+        </a>
+
+        <div>
+
+        </div>
+      </header>
+    </div>
+  )
+}
